a9ded3253e576c08189917892443291b417774bd
Mohammed <Mohammed@gmail.com>
2026-06-11 11:46:41 +0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,22 +6,57 @@ import (
 	"os"
 )
 
+const (
+	host       = "0.0.0.0:4221"
+	bufferSize = 1024
+)
+
+var (
+	response200 = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	response404 = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+)
+
+func ReadRequest(conn net.Conn) ([]byte, error) {
+	buf := make([]byte, bufferSize)
+	_, err := conn.Read(buf)
+	return buf, err
+}
+
+func isRootPath(req []byte) bool {
+	// HTTP request line looks like: "GET / HTTP/1.1"
+	// req[4] == '/' and req[5] == ' ' means the path is exactly "/"
+	return len(req) > 5 && req[4] == '/' && req[5] == ' '
+}
+
+func hundleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	req, err := ReadRequest(conn)
+	if err != nil {
+		fmt.Println("Error when hendle request", err)
+		os.Exit(1)
+	}
+
+	if isRootPath(req) {
+		conn.Write(response200)
+	} else {
+		conn.Write(response404)
+	}
+}
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", host)
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("failed to bind a port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for i := 0; i < 2; i++ {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error Accpeting the message")
+			os.Exit(1)
+		}
+		hundleConnection(conn)
 	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
 }
